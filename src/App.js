46202e90811c78ae0e82408900b11600a9d28a21
nasricoder <nasriamir585@gmail.com>
2022-08-08 13:53:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import NavA from './componnent/NavA';
+import Formulaire from './componnent/Formulaire';
+
+
+
+function App() {
+  return (
+    <div>
+      <NavA />
+      <Formulaire />
+    
+    </div>
+  )
+    
+}
+export default App;
+
+
